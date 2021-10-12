0002ad8724c6425483cad349a030fdd99c72a13d
Zuzana Pelechová <zpelechova@gmail.com>
2021-10-12 13:00:22 +0200

    booking

---
+++ src/another.js
@@ -0,0 +1,333 @@
+const data = [
+  {
+      "_id" : "3o0K5kzDFr4sHKhsg",
+      "actId" : "MiEVd9O3R4Td5AbV9",
+      "userId" : "dETSXgd95XQGhQW8J",
+      "lastRun" : {
+          "_id" : "oFlAPWEqF7xQIInFD",
+          "startedAt" : ISODate("2021-03-21T11:15:45.780Z"),
+          "finishedAt" : ISODate("2021-03-21T13:41:04.261Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 8,
+      "purchasedUntil" : ISODate("2021-03-27T18:13:33.207Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "ske6ddOCg7hSYERer",
+      "actId" : "WeOty7LW7tLRrD2UD",
+      "userId" : "jTgjNFE89M8GE6Fng",
+      "lastRun" : null,
+      "purchasedUntil" : ISODate("2021-05-13T15:28:57.294Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "OV7L7yWOxHpZP6rDf",
+      "actId" : "WeOty7LW7tLRrD2UD",
+      "userId" : "HPMrNRc2D4Ap8rfyP",
+      "lastRun" : {
+          "_id" : "Dqci8HtewJCPMegdS",
+          "startedAt" : ISODate("2021-07-04T14:25:07.673Z"),
+          "finishedAt" : ISODate("2021-07-04T14:25:50.642Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 6,
+      "purchasedUntil" : ISODate("2021-05-16T11:59:11.243Z"),
+      "canceledAt" : null
+  },
+  {
+      "_id" : "bLgRtupaLPdrCfuSk",
+      "actId" : "WeOty7LW7tLRrD2UD",
+      "userId" : "No7agWBxvfWZqo2jQ",
+      "lastRun" : {
+          "_id" : "3paLIZUQxoNjaFuzf",
+          "startedAt" : ISODate("2021-07-26T00:37:27.520Z"),
+          "finishedAt" : ISODate("2021-07-26T00:38:43.720Z"),
+          "status" : "ABORTED"
+      },
+      "totalRuns" : 23,
+      "purchasedUntil" : ISODate("2021-05-19T16:26:17.367Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "HG1fPcO4dntiP5nd8",
+      "actId" : "r8x53fkwxq4EcrQB3",
+      "userId" : "xjiGPgatcdRJmvmbH",
+      "lastRun" : {
+          "_id" : "s5GnOYpWb0Gi0FC8t",
+          "startedAt" : ISODate("2021-07-01T06:15:52.585Z"),
+          "finishedAt" : ISODate("2021-07-01T06:16:15.456Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 4,
+      "purchasedUntil" : ISODate("2021-06-29T07:30:20.686Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "kZK7AaXKdOplf0bDz",
+      "actId" : "QO4eKupwzgl5yAkKY",
+      "userId" : "iKuStJScR8emGdNzn",
+      "lastRun" : {
+          "_id" : "c3QnSIgAw8Z9h0Kaw",
+          "startedAt" : ISODate("2021-10-10T16:30:18.252Z"),
+          "finishedAt" : ISODate("2021-10-10T16:30:46.859Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 124,
+      "purchasedUntil" : ISODate("2021-06-28T18:56:54.505Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "ucnMXD02BPaFmcTOp",
+      "actId" : "GctCXDim0MXeLZegY",
+      "userId" : "bmKwsJht3JBvgEQ9A",
+      "lastRun" : {
+          "_id" : "5u43qjyIwEAL2sgaj",
+          "startedAt" : ISODate("2021-06-29T19:21:41.210Z"),
+          "finishedAt" : ISODate("2021-06-29T19:21:49.781Z"),
+          "status" : "FAILED"
+      },
+      "totalRuns" : 1,
+      "purchasedUntil" : ISODate("2021-07-06T19:21:22.369Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "HplAGh5pBdJWGr6UF",
+      "actId" : "o2bTUjkGaxB6HCypr",
+      "userId" : "AqW8iaySZJaGHiAwn",
+      "lastRun" : {
+          "_id" : "UYdKQxbGiXbVUqu64",
+          "startedAt" : ISODate("2021-08-06T00:08:50.966Z"),
+          "finishedAt" : ISODate("2021-08-06T00:09:06.799Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 25,
+      "purchasedUntil" : ISODate("2021-07-08T19:12:05.596Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "tDzYOtTbLHLdFlTuf",
+      "actId" : "m0caUa21RsTrudP1M",
+      "userId" : "AqW8iaySZJaGHiAwn",
+      "lastRun" : {
+          "_id" : "KZCv3BMpzwzYxGMS9",
+          "startedAt" : ISODate("2021-08-06T00:07:30.115Z"),
+          "finishedAt" : ISODate("2021-08-06T00:13:46.281Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 13,
+      "purchasedUntil" : ISODate("2021-07-11T18:32:13.789Z"),
+      "canceledAt" : null
+  },
+  {
+      "_id" : "Z6caUgyoRV3MLw14T",
+      "actId" : "o2bTUjkGaxB6HCypr",
+      "userId" : "854ZysGCYEWHT37gg",
+      "lastRun" : {
+          "_id" : "Noesoyvf2bTgEthv7",
+          "startedAt" : ISODate("2021-07-21T19:27:57.995Z"),
+          "finishedAt" : ISODate("2021-07-21T19:28:09.592Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 7,
+      "purchasedUntil" : ISODate("2021-07-14T19:01:01.587Z"),
+      "canceledAt" : null
+  },
+  {
+      "_id" : "gubrOl1SwXzdXBwLa",
+      "actId" : "m0caUa21RsTrudP1M",
+      "userId" : "854ZysGCYEWHT37gg",
+      "lastRun" : {
+          "_id" : "ThkbTWt3N0rCMq6cT",
+          "startedAt" : ISODate("2021-07-28T18:09:46.179Z"),
+          "finishedAt" : ISODate("2021-07-28T18:12:37.684Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 14,
+      "purchasedUntil" : ISODate("2021-07-14T18:38:58.082Z"),
+      "canceledAt" : null
+  }
+  ,{
+      "_id" : "TgYuNfdTMqoOCavUb",
+      "actId" : "WeOty7LW7tLRrD2UD",
+      "userId" : "PRoqy7cvZAcv5Ft5w",
+      "lastRun" : {
+          "_id" : "Fmazuestova0bJyq6",
+          "startedAt" : ISODate("2021-07-22T06:54:57.920Z"),
+          "finishedAt" : ISODate("2021-07-22T06:55:16.114Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 22,
+      "purchasedUntil" : ISODate("2021-07-17T12:52:08.683Z"),
+      "canceledAt" : null
+  },
+  {
+      "_id" : "dHI27dkzgiXZZLQg0",
+      "actId" : "GctCXDim0MXeLZegY",
+      "userId" : "aHqDxMPfTy6p9EXQt",
+      "lastRun" : {
+          "_id" : "Qdxfxs0LuivFSnC8e",
+          "startedAt" : ISODate("2021-07-22T18:44:40.698Z"),
+          "finishedAt" : ISODate("2021-07-22T18:47:58.652Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 1,
+      "purchasedUntil" : ISODate("2021-07-29T18:44:06.620Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "slZfBe35YnJ4QfYUQ",
+      "actId" : "dKylGAj0fF0pCjGeW",
+      "userId" : "iEMKdFSBRfcyua8vR",
+      "lastRun" : {
+          "_id" : "TuZDh2aMljKSbfRD6",
+          "startedAt" : ISODate("2021-07-28T17:57:13.379Z"),
+          "finishedAt" : ISODate("2021-07-28T18:42:17.048Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 18,
+      "purchasedUntil" : ISODate("2021-07-31T16:13:56.073Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "emhBhgl9rneKwJBkh",
+      "actId" : "9pUYLfsOvM9dLmNr3",
+      "userId" : "HfSHRwSgKdFZwWqAQ",
+      "lastRun" : {
+          "_id" : "tNvAd5zxjd69DvDRo",
+          "startedAt" : ISODate("2021-08-08T23:46:18.443Z"),
+          "finishedAt" : ISODate("2021-08-08T23:46:59.357Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 1,
+      "purchasedUntil" : ISODate("2021-08-15T23:45:13.429Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "f8z4hvzq9KJ2lyZz3",
+      "userId" : "gvtJuP4yzNZpxKwBR",
+      "actId" : "WeOty7LW7tLRrD2UD",
+      "lastRun" : {
+          "_id" : "4XDQFiSjOkQFblXSd",
+          "startedAt" : ISODate("2021-10-04T15:29:03.381Z"),
+          "finishedAt" : ISODate("2021-10-04T15:29:21.976Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 5339,
+      "purchasedUntil" : ISODate("2021-08-19T12:36:12.943Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "XkLX4uVy5J6iWRHEJ",
+      "actId" : "jBGm27DAI0VE5D6r1",
+      "userId" : "yJML9urtSDEmouE7b",
+      "lastRun" : {
+          "_id" : "IAg5LuoYJ2pRRzH4W",
+          "startedAt" : ISODate("2021-09-14T13:50:18.320Z"),
+          "finishedAt" : ISODate("2021-09-14T15:31:58.963Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 50,
+      "purchasedUntil" : ISODate("2021-08-31T10:26:28.691Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "81DmNXXoheKrx6zwG",
+      "userId" : "No7agWBxvfWZqo2jQ",
+      "actId" : "jBGm27DAI0VE5D6r1",
+      "lastRun" : {
+          "_id" : "MOUbYV07dV9bVy466",
+          "startedAt" : ISODate("2021-08-23T17:06:50.288Z"),
+          "finishedAt" : ISODate("2021-08-23T17:19:51.692Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 2,
+      "purchasedUntil" : ISODate("2021-09-07T16:57:24.290Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "2QhpZzdJKJmfoA9wb",
+      "actId" : "9pUYLfsOvM9dLmNr3",
+      "userId" : "mCZyH3C89xN3rEaLm",
+      "lastRun" : {
+          "_id" : "rBhpbpO477muzSaoc",
+          "startedAt" : ISODate("2021-08-30T21:30:49.290Z"),
+          "finishedAt" : ISODate("2021-08-30T21:44:47.730Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 19,
+      "purchasedUntil" : ISODate("2021-09-01T20:05:40.992Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "m3tY7LnPvjH7LR7gt",
+      "userId" : "fWLDGEe6ER8fDzbHf",
+      "actId" : "t05URlYvxiHpMe5cv",
+      "lastRun" : {
+          "_id" : "aROCUiuhEl2dw3SF5",
+          "startedAt" : ISODate("2021-10-11T07:22:03.229Z"),
+          "finishedAt" : ISODate("2021-10-11T07:22:12.615Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 479,
+      "purchasedUntil" : ISODate("2021-09-13T10:35:51.935Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "W2Ob2WO4jnPS7bAic",
+      "actId" : "GctCXDim0MXeLZegY",
+      "userId" : "5amuQmTpDL2wE7veX",
+      "lastRun" : {
+          "_id" : "d7RBrGBJO4H8fqWtY",
+          "startedAt" : ISODate("2021-09-09T12:33:46.287Z"),
+          "finishedAt" : ISODate("2021-09-09T22:42:24.606Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 9,
+      "purchasedUntil" : ISODate("2021-09-15T10:43:33.001Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "AfGGgpFw7aCNcrpu8",
+      "userId" : "P9pJBKrFs9CKDpEyJ",
+      "actId" : "MiEVd9O3R4Td5AbV9",
+      "lastRun" : {
+          "_id" : "eBfgr8KMEdDFy5R5S",
+          "startedAt" : ISODate("2021-10-06T15:43:31.872Z"),
+          "finishedAt" : ISODate("2021-10-06T15:52:44.235Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 11,
+      "purchasedUntil" : ISODate("2021-09-23T19:27:18.459Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "texxVKL7TqjdqOrep",
+      "actId" : "0mFoOGFw7K4hp1v0f",
+      "userId" : "J5XuEe5jTbC7GJk8T",
+      "lastRun" : {
+          "_id" : "NPBHXw8BQuPZ4vTCF",
+          "startedAt" : ISODate("2021-09-27T05:32:03.256Z"),
+          "finishedAt" : ISODate("2021-09-27T05:33:46.226Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 82,
+      "purchasedUntil" : ISODate("2021-10-01T00:01:41.973Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "bjKYJMwYgK1WJfKd6",
+      "actId" : "0mFoOGFw7K4hp1v0f",
+      "userId" : "WNqctW4R39FKuoYJb",
+      "lastRun" : {
+          "_id" : "HJtnV1yNLzPXRZmQL",
+          "startedAt" : ISODate("2021-10-10T23:27:57.697Z"),
+          "finishedAt" : ISODate("2021-10-10T23:39:27.176Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 3,
+      "purchasedUntil" : ISODate("2021-10-07T05:22:00.971Z"),
+      "canceledAt" : null
+  },{
+      "_id" : "ukKmagosgBv5mgauK",
+      "actId" : "t05URlYvxiHpMe5cv",
+      "userId" : "KJvnQnXiYgYhzvmQw",
+      "lastRun" : {
+          "_id" : "WSDWNIN2FaPakQ2Ik",
+          "startedAt" : ISODate("2021-10-09T09:31:41.898Z"),
+          "finishedAt" : ISODate("2021-10-09T09:31:50.072Z"),
+          "status" : "SUCCEEDED"
+      },
+      "totalRuns" : 5,
+      "purchasedUntil" : ISODate("2021-10-16T09:14:11.621Z"),
+      "canceledAt" : null
+  }]
